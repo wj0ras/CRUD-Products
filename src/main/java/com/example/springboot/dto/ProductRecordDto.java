66f9ff56267fc54fad6fde47b00f34d12e12381a
Welison Joras <welison.joras@hotmail.com>
2024-04-10 00:07:44 -0300

    created project

---
+++ src/main/java/com/example/springboot/dto/ProductRecordDto.java
@@ -0,0 +1,13 @@
+package com.example.springboot.dto;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+	
+	
+
+	
+}
---
+++ src/main/java/com/example/springboot/model/ProductModel.java
@@ -0,0 +1,42 @@
+package com.example.springboot.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_PRODUCTS")
+public class ProductModel implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private UUID id;
+	private String name;
+	private BigDecimal value;
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+}
